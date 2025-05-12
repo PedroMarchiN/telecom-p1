5de9bda307c57b1a82ee5156ea833541670e0e0b
PedroMarchiN <pedromarchinunes@estudante.ufscar.br>
2025-05-12 14:08:09 -0300

    a

--- uart.cpp
+++ uart.cpp
@@ -25,7 +25,7 @@ void UART_RX::put_samples(const unsigned int *buffer, unsigned int n)
                     sample_index = 0;
                     bit_index    = 0;
                     current_byte = 0;
-                    wait_for     = SAMPLES_PER_SYMBOL + (SAMPLES_PER_SYMBOL / 2);
+                    wait_for     = SAMPLES_PER_SYMBOL/2;
                 }
             }
         }
