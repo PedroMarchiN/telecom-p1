4b8554c4c358f969b35ae5161a5b5bfe83ca5067
PedroMarchiN <pedromarchinunes@estudante.ufscar.br>
2025-05-12 13:31:36 -0300

    Mudança do wait_for, deslocando 1 bit para o lado

--- uart.cpp
+++ uart.cpp
@@ -35,7 +35,7 @@ void UART_RX::put_samples(const unsigned int *buffer, unsigned int n)
                     sample_index = 0;
                     bit_index = 0;
                     current_byte = 0;
-                    wait_for = SAMPLES_PER_SYMBOL / 2;  
+                    wait_for = (SAMPLES_PER_SYMBOL / 2) + 1; 
                     window.clear();
                 }
             }
