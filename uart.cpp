af1ad5139f23d8d6f6f38add88a35374dfe9c0ab
PedroMarchiN <pedromarchinunes@estudante.ufscar.br>
2025-06-07 14:57:49 -0300

    Teste duplamento inválido

--- uart.cpp
+++ uart.cpp
@@ -2,7 +2,50 @@
 
 void UART_RX::put_samples(const unsigned int *buffer, unsigned int n)
 {
-    // seu código aqui
+    for (unsigned int i = 0; i < n; i++) {
+        this->samples.push_front(buffer[i]);
+        if (this->samples[0] == 0)
+            this->low_bit_counter++;
+        if (this->samples[30] == 0)
+            this->low_bit_counter--; // low bit leaving the window 
+
+        switch (state) {
+            case IDLE:
+                if (low_bit_counter >= 25 && this->samples[93] == 0) {
+                    // This is a start bit!
+                    this->cycles_counter = 15; // after midbit (79 out of 160)
+                    this->byte = 0;
+                    this->bits_read = 0;
+                    this->state = DATA_BIT;
+                }
+
+                break;
+
+            case DATA_BIT:
+                if (this->cycles_counter == 159) {
+                    this->byte += this->samples[0] << this->bits_read;
+                    this->bits_read++;
+                    this->cycles_counter = 0;
+                    if (this->bits_read == 8) 
+                        this->state = STOP_BIT;
+                } else
+                    this->cycles_counter++; 
+
+                break;
+
+            case STOP_BIT:
+                if (this->cycles_counter == 159) {
+                    this->get_byte(this->byte);
+                    this->state = IDLE;
+                } else
+                    this->cycles_counter++;
+                break;
+
+            default: break;
+        }
+
+        this->samples.pop_back();
+    }
 }
 
 void UART_TX::put_byte(uint8_t byte)
