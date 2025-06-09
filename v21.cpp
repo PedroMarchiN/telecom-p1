cf56f2d5fdf19b4ec63bcbc908f28abc034d5a64
PedroMarchiN <pedromarchinunes@estudante.ufscar.br>
2025-06-08 21:47:27 -0300

    Correções gerais

--- v21.cpp
+++ v21.cpp
@@ -4,17 +4,14 @@
 #include <numbers>
 #include "config.hpp"
 
-// Implementação do receptor
 V21_RX::V21_RX(float omega_mark, float omega_space, 
                std::function<void(const unsigned int *, unsigned int)> get_digital_samples) :
     omega_mark(omega_mark),
     omega_space(omega_space),
     get_digital_samples(get_digital_samples)
 {
-    // Preenchimento inicial do buffer
     sample_buffer = std::deque<float>(SAMPLES_PER_SYMBOL, 0.0f);
     
-    // Coeficientes do filtro passa-baixas (Butterworth)
     lp_numerator[0] = 0.00037506961629696616f;
     lp_numerator[1] = 0.0007501392325939323f;
     lp_numerator[2] = 0.00037506961629696616f;
@@ -22,14 +19,11 @@ V21_RX::V21_RX(float omega_mark, float omega_space,
     lp_denominator[1] = -1.9444776577670935f;
     lp_denominator[2] = 0.9459779362322813f;
 
-    // Pré-cálculo das constantes dos filtros ressonantes
     const float BANDPASS_SMOOTHING = 0.99f;
     const int L = SAMPLES_PER_SYMBOL;
     
-    rl_cos_space = std::pow(BANDPASS_SMOOTHING, L) * 
-                   std::cos(omega_space * L * SAMPLING_PERIOD);
-    rl_sin_space = std::pow(BANDPASS_SMOOTHING, L) * 
-                   std::sin(omega_space * L * SAMPLING_PERIOD);
+    rl_cos_space = std::pow(BANDPASS_SMOOTHING, L) * std::cos(omega_space * L * SAMPLING_PERIOD);
+    rl_sin_space = std::pow(BANDPASS_SMOOTHING, L) * std::sin(omega_space * L * SAMPLING_PERIOD);
     r_cos_space = BANDPASS_SMOOTHING * std::cos(omega_space * SAMPLING_PERIOD);
     r_sin_space = BANDPASS_SMOOTHING * std::sin(omega_space * SAMPLING_PERIOD);
     
@@ -47,49 +41,31 @@ void V21_RX::demodulate(const float *in_analog_samples, unsigned int n)
     const int L = SAMPLES_PER_SYMBOL;
 
     for (unsigned int i = 0; i < n; ++i) {
-        // Atualiza buffer de amostras
         sample_buffer.push_front(in_analog_samples[i]);
         sample_buffer.pop_back();
         
-        // Filtro ressonante para espaço (1650 Hz)
-        float vspace_r = sample_buffer[0] - rl_cos_space * sample_buffer[L] +
-                         r_cos_space * vspace_r_buffer - r_sin_space * vspace_i_buffer;
-        float vspace_i = -rl_sin_space * sample_buffer[L] + 
-                         r_cos_space * vspace_i_buffer + r_sin_space * vspace_r_buffer;
-        
-        // Filtro ressonante para marca (1850 Hz)
-        float vmark_r = sample_buffer[0] - rl_cos_mark * sample_buffer[L] +
-                        r_cos_mark * vmark_r_buffer - r_sin_mark * vmark_i_buffer;
-        float vmark_i = -rl_sin_mark * sample_buffer[L] + 
-                        r_cos_mark * vmark_i_buffer + r_sin_mark * vmark_r_buffer;
-        
-        // Atualiza estados dos filtros
+        float vspace_r = sample_buffer[0] - rl_cos_space * sample_buffer[L] + r_cos_space * vspace_r_buffer - r_sin_space * vspace_i_buffer;
+        float vspace_i = -rl_sin_space * sample_buffer[L] +  r_cos_space * vspace_i_buffer + r_sin_space * vspace_r_buffer;
+        float vmark_r = sample_buffer[0] - rl_cos_mark * sample_buffer[L] + r_cos_mark * vmark_r_buffer - r_sin_mark * vmark_i_buffer;
+        float vmark_i = -rl_sin_mark * sample_buffer[L] + r_cos_mark * vmark_i_buffer + r_sin_mark * vmark_r_buffer;
+
         vspace_r_buffer = vspace_r;
         vspace_i_buffer = vspace_i;
         vmark_r_buffer = vmark_r;
         vmark_i_buffer = vmark_i;
         
-        // Calcula diferença de energia
         float raw_decision = (vmark_r * vmark_r + vmark_i * vmark_i) -
                              (vspace_r * vspace_r + vspace_i * vspace_i);
-        
-        // Filtragem passa-baixas
-        float filtered_decision = 
-            lp_numerator[0] * raw_decision +
-            lp_numerator[1] * raw_decision_buffer[0] +
-            lp_numerator[2] * raw_decision_buffer[1] -
-            lp_denominator[1] * filtered_decision_buffer[0] -
-            lp_denominator[2] * filtered_decision_buffer[1];
+
+        float filtered_decision =  lp_numerator[0] * raw_decision + lp_numerator[1] * raw_decision_buffer[0] + lp_numerator[2] * raw_decision_buffer[1] - lp_denominator[1] * filtered_decision_buffer[0] - lp_denominator[2] * filtered_decision_buffer[1];
         
         filtered_decision /= lp_denominator[0];
         
-        // Atualiza buffers de decisão
         raw_decision_buffer[1] = raw_decision_buffer[0];
         raw_decision_buffer[0] = raw_decision;
         filtered_decision_buffer[1] = filtered_decision_buffer[0];
         filtered_decision_buffer[0] = filtered_decision;
         
-        // Máquina de estados para detecção de portadora
         switch (state) {
             case IDLE:
                 if (std::abs(filtered_decision) > 120.0f) {
@@ -97,7 +73,7 @@ void V21_RX::demodulate(const float *in_analog_samples, unsigned int n)
                     low_difference_counter = 0;
                     state = CARRIER_DETECTED;
                 } else {
-                    digital_samples[i] = 1; // Linha ociosa
+                    digital_samples[i] = 1; 
                 }
                 break;
                 
@@ -121,7 +97,6 @@ void V21_RX::demodulate(const float *in_analog_samples, unsigned int n)
     get_digital_samples(digital_samples, n);
 }
 
-// Implementação do transmissor
 void V21_TX::modulate(const unsigned int *in_digital_samples, 
                       float *out_analog_samples, 
                       unsigned int n)
--- v21.hpp
+++ v21.hpp
@@ -2,55 +2,49 @@
 #define V21_HPP
 
 #include <functional>
-#include <array>
+#include <deque>
 #include <cmath>
-#include <numeric> // Para std::accumulate
+#include <numbers>
 #include "config.hpp"
 
-class V21_RX {
+class V21_RX
+{
 public:
-    V21_RX(float omega_mark, float omega_space, 
-           std::function<void(const unsigned int*, unsigned int)> get_digital_samples);
-    void demodulate(const float* in_analog_samples, unsigned int n);
+    V21_RX(float omega_mark, float omega_space, std::function<void(const unsigned int *, unsigned int)> get_digital_samples);
+    void demodulate(const float *in_analog_samples, unsigned int n);
     ~V21_RX() = default;
-
 private:
-    const float MARK_FREQ;
-    const float SPACE_FREQ;
-    std::function<void(const unsigned int*, unsigned int)> get_digital_samples;
+    float omega_mark, omega_space;
+    std::function<void(const unsigned int *, unsigned int)> get_digital_samples;
+
+    float rl_cos_space, rl_sin_space, r_cos_space, r_sin_space;
+    float rl_cos_mark, rl_sin_mark, r_cos_mark, r_sin_mark;
     
-    static constexpr size_t FIR_TAPS = 31;
-    std::array<float, FIR_TAPS> fir_coeffs;
+    float lp_numerator[3];
+    float lp_denominator[3];
     
-    std::array<float, SAMPLES_PER_SYMBOL> delay_line;
-    size_t delay_index = 0;
-    float last_i_mark = 0.0f, last_q_mark = 0.0f;
-    float last_i_space = 0.0f, last_q_space = 0.0f;
-    float energy_history[5] = {0};
-    size_t energy_index = 0;
+
+    std::deque<float> sample_buffer;
+    float vspace_r_buffer = 0.0f, vspace_i_buffer = 0.0f;
+    float vmark_r_buffer = 0.0f, vmark_i_buffer = 0.0f;
+    float raw_decision_buffer[2] = {0.0f, 0.0f};
+    float filtered_decision_buffer[2] = {0.0f, 0.0f};
     
-    enum State { NO_CARRIER, CARRIER_PRESENT } state = NO_CARRIER;
-    unsigned int low_energy_count = 0;
-    
-    float process_sample(float sample);
-    void update_energy(float energy);
-    bool carrier_detected();
+    enum State { IDLE, CARRIER_DETECTED } state = IDLE;
+    unsigned int low_difference_counter = 0;
 };
 
-class V21_TX {
+class V21_TX
+{
 public:
     V21_TX(float omega_mark, float omega_space) : 
-        MARK_FREQ(omega_mark), 
-        SPACE_FREQ(omega_space), 
-        phase(0.0f) {}
+        omega_mark(omega_mark), 
+        omega_space(omega_space), 
+        phase(0.f) {};
     
-    void modulate(const unsigned int* in_digital_samples, 
-                 float* out_analog_samples, 
-                 unsigned int n);
-
+    void modulate(const unsigned int *in_digital_samples, float *out_analog_samples, unsigned int n);
 private:
-    const float MARK_FREQ;
-    const float SPACE_FREQ;
+    float omega_mark, omega_space;
     float phase;
 };
 
