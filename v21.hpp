c76e2999fb380ba37ff3a129ef6d1d5e325c8f69
PedroMarchiN <pedromarchinunes@estudante.ufscar.br>
2025-06-08 21:25:07 -0300

    Testes finais

--- v21.hpp
+++ v21.hpp
@@ -2,52 +2,59 @@
 #define V21_HPP
 
 #include <functional>
-#include <deque>
+#include <array>
 #include <cmath>
-#include <numbers>
 #include "config.hpp"
 
 class V21_RX
 {
 public:
-    V21_RX(float omega_mark, float omega_space, std::function<void(const unsigned int *, unsigned int)> get_digital_samples);
+    V21_RX(float omega_mark, float omega_space, 
+           std::function<void(const unsigned int *, unsigned int)> get_digital_samples);
     void demodulate(const float *in_analog_samples, unsigned int n);
     ~V21_RX() = default;
+
 private:
-    float omega_mark, omega_space;
+    const float MARK_FREQ;
+    const float SPACE_FREQ;
     std::function<void(const unsigned int *, unsigned int)> get_digital_samples;
     
-    // Constantes pré-calculadas para os filtros ressonantes
-    float rl_cos_space, rl_sin_space, r_cos_space, r_sin_space;
-    float rl_cos_mark, rl_sin_mark, r_cos_mark, r_sin_mark;
+    static constexpr size_t FIR_TAPS = 31;
+    std::array<float, FIR_TAPS> fir_coeffs;
     
-    // Coeficientes do filtro passa-baixas
-    float lp_numerator[3];
-    float lp_denominator[3];
+    std::array<float, SAMPLES_PER_SYMBOL> delay_line;
+    size_t delay_index = 0;
+    float last_i_mark = 0.0f;
+    float last_q_mark = 0.0f;
+    float last_i_space = 0.0f;
+    float last_q_space = 0.0f;
+    float phase_accumulator = 0.0f;
+    float energy_history[5] = {0};
+    size_t energy_index = 0;
     
-    // Buffers de estado
-    std::deque<float> sample_buffer;
-    float vspace_r_buffer = 0.0f, vspace_i_buffer = 0.0f;
-    float vmark_r_buffer = 0.0f, vmark_i_buffer = 0.0f;
-    float raw_decision_buffer[2] = {0.0f, 0.0f};
-    float filtered_decision_buffer[2] = {0.0f, 0.0f};
-    
-    // Máquina de estados para detecção de portadora
-    enum State { IDLE, CARRIER_DETECTED } state = IDLE;
-    unsigned int low_difference_counter = 0;
+    enum State { NO_CARRIER, CARRIER_PRESENT } state = NO_CARRIER;
+    unsigned int low_energy_count = 0;
+
+    float process_sample(float sample);
+    void update_energy(float energy);
+    bool carrier_detected();
 };
 
 class V21_TX
 {
 public:
     V21_TX(float omega_mark, float omega_space) : 
-        omega_mark(omega_mark), 
-        omega_space(omega_space), 
-        phase(0.f) {};
+        MARK_FREQ(omega_mark), 
+        SPACE_FREQ(omega_space), 
+        phase(0.0f) {}
     
-    void modulate(const unsigned int *in_digital_samples, float *out_analog_samples, unsigned int n);
+    void modulate(const unsigned int *in_digital_samples, 
+                 float *out_analog_samples, 
+                 unsigned int n);
+
 private:
-    float omega_mark, omega_space;
+    const float MARK_FREQ;
+    const float SPACE_FREQ;
     float phase;
 };
 
